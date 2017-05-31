605c9791adef5aae769ed5de33ee3b834b54fedf
Joshua Burleson <Joshua Burleson>
2017-05-31 13:40:57 -0400

    removed erroneous directory

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/',function(req,res){
+    var parsedHeadObj = {ipaddress : '',
+                         language : '',
+                         software : '',
+                        };
+    parsedHeadObj.ipaddress = req.connection.remoteAddress;
+    parsedHeadObj.language = req.headers['accept-language'].split(',')[0];
+    parsedHeadObj.software = req.headers['user-agent'].split('(')[1].split(')')[0];
+    res.end(JSON.stringify(parsedHeadObj));
+});
+
+app.listen(port);   
