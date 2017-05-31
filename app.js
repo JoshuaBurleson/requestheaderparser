973d84bbc37b2da38432a67705a23a51960b9aed
Joshua Burleson <Joshua Burleson>
2017-05-31 14:29:44 -0400

    added header and title to response

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.get('/',function(req,res){
     parsedHeadObj.ipaddress = req.header('x-forwarded-for') || req.connection.remoteAddress;
     parsedHeadObj.language = req.headers['accept-language'].split(',')[0];
     parsedHeadObj.software = req.headers['user-agent'].split('(')[1].split(')')[0];
+    res.writeHead(200, 'text/html');
+    res.write('<title>Request Header Parser</title>');
     res.end(JSON.stringify(parsedHeadObj));
 });
 
